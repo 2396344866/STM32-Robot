e6aed89a6fa4fe42fbd4f1a947d8b4e34702f1f0
2396344866 <167438957+2396344866@users.noreply.github.com>
2026-08-21 21:33:56 +0800

    chore: 解除跟踪 Keil 构建/调试产物与机器文件，规范 .gitignore
    - git rm --cached: Objects/ Listings/ RTE/ DebugConfig/
    - git rm --cached: project.uvguix.* project.uvoptx EventRecorderStub.scvd
    - .gitignore 新增: docs/test.md(私密) *.uvguix.* *.uvoptx DebugConfig/ *.scvd RTE/ *_backup_*/ 编辑器&OS 兜底
    - 本地文件保留，仅解除版本库跟踪

--- Objects/dev_dht11.d
+++
@@ -1,3 +0,0 @@
-.\objects\dev_dht11.o: DD_Device_Driver\dev_dht11.c
-.\objects\dev_dht11.o: DD_Device_Driver\dev_dht11.h
-.\objects\dev_dht11.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- Objects/dev_led.d
+++
@@ -1,4 +0,0 @@
-.\objects\dev_led.o: DD_Device_Driver\dev_led.c
-.\objects\dev_led.o: DD_Device_Driver\dev_led.h
-.\objects\dev_led.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\dev_led.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
